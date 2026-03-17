a75bd6fb3f52699aa5ce6309d49ef4b1f3eb3a33
Vansh Malik <vansh042006@gmail.com>
2026-03-17 17:41:02 +0530

    reCommmit server

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const port = 3000;
 await connectDB();
 
 // Stripe webhook endpoint (must be BEFORE express.json())
-app.post('/api/stripe', express.raw({type: 'application/json'}), stripeWebhooks);
+app.post('/api/stripe', express.raw({type: 'application/json'}), stripeWebHooks);
 
 //Middleware
 app.use(cors());
